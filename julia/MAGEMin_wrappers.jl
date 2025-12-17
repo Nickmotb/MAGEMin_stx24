f8df0f552d8af7286d53cf38011cd62aba855a17
Nicolas Riel <a.nicolas.riel@gmail.com>
2025-12-17 11:33:48 +0100

    corrected wt2mol

--- julia/MAGEMin_wrappers.jl
+++ julia/MAGEMin_wrappers.jl
@@ -947,8 +947,8 @@ Converts bulk-rock composition from wt to mol fraction
 function wt2mol(    bulk_wt     :: AbstractVector{Float64},
                     bulk_ox     :: AbstractVector{String}) 
 
-    ref_ox          = ["SiO2"; "Al2O3"; "CaO"; "MgO"; "FeO"; "Fe2O3"; "K2O"; "Na2O"; "TiO2"; "O"; "Cr2O3"; "MnO"; "H2O"; "CO2"; "S"];
-    ref_MolarMass   = [60.08; 101.96; 56.08; 40.30; 71.85; 159.69; 94.2; 61.98; 79.88; 16.0; 151.99; 70.937; 18.015; 44.01; 32.06];      #Molar mass of oxides
+    ref_ox          = ["SiO2"; "Al2O3"; "CaO"; "MgO"; "FeO"; "Fe2O3"; "K2O"; "Na2O"; "TiO2"; "O"; "Cr2O3"; "MnO"; "H2O"; "CO2"; "S"; "Fe"];
+    ref_MolarMass   = [60.08; 101.96; 56.08; 40.30; 71.85; 159.69; 94.2; 61.98; 79.88; 16.0; 151.99; 70.937; 18.015; 44.01; 32.06; 55.85];      #Molar mass of oxides
 
     bulk_mol = zeros(length(bulk_ox));
     bulk_wt  = normalize(bulk_wt)
